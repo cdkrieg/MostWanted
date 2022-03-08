122336967c49275a3ccd9efd63f2581ad8494386
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-03-08 17:13:11 -0600

    added a new function for validation and added inderx.html to gitnoire

--- app.js
+++ app.js
@@ -534,5 +534,17 @@ function dateValidation(input){
   }
 }
 
+function testValidation(people, input, trait) {
+  let found = false;
+  //
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].firstName == input) {
+      found = true;
+      break;
+    }
+  }
+  return found
+}
+
 
 //#endregion
