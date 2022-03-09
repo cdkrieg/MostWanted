0cb2de0637de0d41ecb75ca50bd8e2e7c2d75b0d
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-03-08 18:14:51 -0600

    finished validation for the traits list now working on the user inputs on traits

--- test.js
+++ test.js
@@ -1,11 +1,46 @@
-function testValidation(people, input, trait) {
+function test(people) {
+  let input = "Billy";
+  let trait = "firstName";
+  let results = testValidation(people, input, trait);
+  console.log(results)
+}
+
+function testValidation(people, input, traits) {
+  let trait = traitsValidation(traits)
+  let key = 1;
   let found = false;
-  //
-  for (let i = 0; i < people.length; i++) {
-    if (people[i].firstName == input) {
-      found = true;
-      break;
+  const keys = [
+    "id",
+    "firstName",
+    "lastName",
+    "gender",
+    "dob",
+    "height",
+    "weight",
+    "eyeColor",
+    "occupation",
+    "parents",
+    "currentSpouse",
+  ];
+
+  if (keys.includes(trait)) {
+    for (let i = 0; i < people.length; i++) {
+      if (people[i][keys[key]] == input) {
+        found = true;
+        break;
+      }
+      
     }
+  } else {
+    alert("Not valid entry");
+    return testValidation(people, input, trait);
   }
-  return found
+  return found;
+}
+
+
+function traitsValidation(trait) {
+  
+
+  
 }
